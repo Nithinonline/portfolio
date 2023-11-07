dc711b249f183296e5c544a3cad9ebde22148de1
NITHIN <nithinonline00055@gmail.com>
2023-11-07 23:48:51 +0530

    Corrected projects

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -26,17 +26,18 @@ export const Contact = () => {
                         <img
                             src="/images/linkedinIcon.png"
                             alt="LinkedIn icon"
+                            className={styles.img}
                         />
                         <a href="https://www.linkedin.com/in/webdev-nithin-suresh/" target='_blank'>linkedin.com/NITHIN SURESH</a>
                     </li>
 
                     <li className={styles.link}>
-                        <img src="/images/githubIcon.png" alt="Github icon" />
+                        <img src="/images/githubIcon.png" alt="Github icon"  className={styles.img} />
                         <a href="https://github.com/Nithinonline" target='_blank'>github.com/Nithinonline</a>
                     </li>
 
                     <li className={styles.link}>
-                        <img src="/images/IG.png" alt="IG icon" />
+                        <img src="/images/IG.png" alt="IG icon"  className={styles.img} />
                         <a href="https://www.instagram.com/nithinify/" target='_blank'>instagram.com/nithinify</a>
                     </li>
 
